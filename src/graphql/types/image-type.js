60dab6b9fad238f0d5e33fd3646d04605745bfb0
Maksym Cherniavskyi <morfey897@gmail.com>
2020-10-02 09:29:51 +0300

    Change image type

--- src/graphql/types/image-type.js
+++ src/graphql/types/image-type.js
@@ -1,10 +1,11 @@
 
-const { GraphQLObjectType, GraphQLString } = require('graphql');
+const { GraphQLObjectType, GraphQLString, GraphQLID } = require('graphql');
 
 const ImageType = new GraphQLObjectType({
     name: 'ImageType',
     description: "This is image type",
     fields: () => ({
+      _id: { type: GraphQLID },
       url: {type: GraphQLString},
     })
 });
